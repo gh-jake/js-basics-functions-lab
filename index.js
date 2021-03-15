943b84b742033fda713e8eff423efdcafdf2f258
Jake Houston <jakechouston@gmail.com>
2021-03-15 19:31:13 -0400

    commit tree 3ef92ac62cc2b0a6d6f4b3c39f6181607757cb2c

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupBlock) {
+    return Math.abs(pickupBlock - 42);
+}
+
+function distanceFromHqInFeet(pickupBlock) {
+    return distanceFromHqInBlocks(pickupBlock) * 264;
+}
+
+function distanceTravelledInFeet(pickupBlock, dropoffBlock) {
+    return Math.abs(dropoffBlock - pickupBlock) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let feetTravelled; 
+    feetTravelled = distanceTravelledInFeet(start, destination);
+    
+    if(feetTravelled <= 400) {
+        return 0;
+    }
+    else {
+        feetTravelled = feetTravelled - 400;
+
+        if(feetTravelled <= 1600) {
+            return feetTravelled * .02;
+        }
+        else if(feetTravelled > 1600 && feetTravelled <= 2100) {
+            return 25;
+        }
+        else {
+            return 'cannot travel that far';
+        }
+    }
+          
+    
+}
